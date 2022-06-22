fb9e957bccf6811ce64b1182041c661eb939254e
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-06-22 16:46:03 -0500

    Consolidating Helium SDK Delegates
    - Added Delegate.cs
    
    Following delegates were created:
    * HeliumEvent
    * HeliumILRDEvent
    * HeliumPlacementEvent
    * HeliumBidEvent
    * HeliumRewardEvent

---
+++ Runtime/Delegates.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+
+namespace Helium
+{
+    /// <param name="error">The error encountered, if any.</param>
+    public delegate void HeliumEvent(HeliumError error);
+
+    /// <param name="placement">The placement the Helium ad.</param>
+    /// <param name="impressionData">The impression data delivered within a hashtable.</param>
+    // ReSharper disable once InconsistentNaming
+    // ReSharper disable once IdentifierTypo
+    public delegate void HeliumILRDEvent(string placement, Hashtable impressionData);
+
+    /// <summary>
+    /// <param name="placement">The placement for the placement.</param>
+    /// <param name="error">The error encountered, if any.</param>
+    /// </summary>
+    public delegate void HeliumPlacementEvent(string placement, HeliumError error);
+    
+    /// <summary>
+    /// <param name="placement">The placement for the placement.</param>
+    /// <param name="bid">The bid information, if any.</param>
+    /// </summary>
+    public delegate void HeliumBidEvent(string placement, HeliumBidInfo bid);
+
+    /// <summary>
+    /// <param name="reward">The reward information.</param>
+    /// </summary>
+    public delegate void HeliumRewardEvent(string reward);
+}
