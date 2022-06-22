f74566d61762dc6d452b2e747c11b7f08b03ac8e
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-06-22 16:46:43 -0500

    Creating Constructor in HeliumBidInfo.cs

--- Runtime/HeliumBidinfo.cs
+++ Runtime/HeliumBidinfo.cs
@@ -6,5 +6,13 @@
         public string PartnerPlacementName;
         public double Price;
         public string Seat;
+
+        public HeliumBidInfo(string auctionId, string partnerPlacementName, double price, string seat)
+        {
+            AuctionId = auctionId;
+            PartnerPlacementName = partnerPlacementName;
+            Price = price;
+            Seat = seat;
+        }
     }
 }
