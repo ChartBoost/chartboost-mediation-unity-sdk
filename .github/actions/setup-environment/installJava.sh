6c32e781848d644635bb991c9494200e161f7919
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-06-29 12:24:28 -0500

    Simplifying Initial Setup

--- .github/actions/setup-environment/installJava.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Java 11 and make it the default
-brew tap AdoptOpenJDK/openjdk
-brew install --cask adoptopenjdk11 --quiet
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
-exec bash
