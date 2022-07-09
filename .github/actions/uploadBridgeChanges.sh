325b4cf220dc2778cfdf871af17ebf87e2f31476
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-07-09 05:09:18 -0500

    Improving Git Logic

--- .github/actions/uploadBridgeChanges.sh
+++ .github/actions/uploadBridgeChanges.sh
@@ -1,3 +1,6 @@
+SUCCESS_PUSH="Successfuly Pushed Android Bridge Changes"
+FAILED_PUSH="Failed to Push Android Bridge Changes"
+
 git add "com.chartboost.helium/Runtime/Plugins/Android/helium-android-bridge.jar"
 
 if ! git commit -m 'Android Bridge Changes Found! Updating Bridge'
@@ -5,11 +8,25 @@ then
   echo 'No Android Bridge Changes Found, Nothing to Commit!'
 else
   echo "Android Bridge Changes Found, Attempting Push!"
-  if git push -f origin HEAD:"${GITHUB_HEAD_REF}"
+
+  # if we are on a pull request, find the HEAD_REF and force push our bridge changes
+  if [ -z "$GITHUB_HEAD_REF" ];
   then
-    echo "Successfuly Pushed Android Bridge Changes"
+    if git push -f origin HEAD:"${GITHUB_HEAD_REF}"
+    then
+      echo $SUCCESS_PUSH
+    else
+      echo $FAILED_PUSH
+      exit 1
+    fi
+  # if we are on a branch, no need to do anything
   else
-    echo "Failed to Push Android Bridge Changes"
-    exit 1
-  fi
+    if git push
+    then
+      echo $SUCCESS_PUSH
+    else
+      echo $FAILED_PUSH
+      exit 1
+    fi
+  if
 fi
