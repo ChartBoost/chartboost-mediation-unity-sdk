672695ec496de55f5eec315ec2d2c1bd4fc6a63a
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-10-11 13:39:11 -0500

    Importing Samples on Version Change

--- com.chartboost.helium/Editor/HeliumSetupChecker.cs
+++ com.chartboost.helium/Editor/HeliumSetupChecker.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
@@ -9,9 +11,10 @@ namespace Editor
 {
     public class HeliumSetupChecker
     {
+        private const string Helium = "Helium";
         private const string HeliumWindowTitle = "Helium Unity SDK - Integration Status Checker";
         private const string HeliumPackageName = "com.chartboost.helium";
-        private const string HeliumSamplesAssets = "Assets/Samples/Helium SDK";
+        private const string HeliumSamplesInAssets = "Assets/Samples/Helium SDK";
 
         private static PackageInfo FindPackage(string packageName)
         {
@@ -25,9 +28,9 @@ namespace Editor
         [MenuItem("Helium/Check Integration")]
         public static void CheckHeliumIntegration()
         {
-            if (Directory.Exists(HeliumSamplesAssets))
+            if (Directory.Exists(HeliumSamplesInAssets))
             {
-                var subDirectories = Directory.GetDirectories(HeliumSamplesAssets);
+                var subDirectories = Directory.GetDirectories(HeliumSamplesInAssets);
                 if (subDirectories.Length <= 0)
                 {
                     EditorUtility.DisplayDialog(
@@ -52,7 +55,17 @@ namespace Editor
                         return;
                     }
                     
+                    var helium = FindPackage(HeliumPackageName);
+
                     var optionalDependencies = Directory.GetDirectories(versionDirectory);
+
+                    var importedDependencies = new HashSet<string>();
+
+                    foreach (var imported in optionalDependencies)
+                    {
+                        var sampleName = Path.GetFileName(imported);
+                        importedDependencies.Add(sampleName);
+                    }
 
                     if (optionalDependencies.Length <= 0)
                     {
@@ -63,10 +76,23 @@ namespace Editor
                     } 
                     else
                     {
-                        Debug.LogError("At least one adapter has been added");
+                        var allSamples = Sample.FindByPackage(HeliumPackageName, helium.version);
+
+                        if (!importedDependencies.Contains(Helium))
+                        {
+                            var addHeliumDependencies = EditorUtility.DisplayDialog(
+                                HeliumWindowTitle, 
+                                $"Helium Dependencies not found in Assets.\n\nWould you like to add them?",
+                                "Yes", "No");
+
+                            if (addHeliumDependencies)
+                            {
+                                var heliumSample = allSamples.Single(x => x.displayName.Equals(Helium));
+                                heliumSample.Import(Sample.ImportOptions.HideImportWindow |
+                                                    Sample.ImportOptions.OverridePreviousImports);
+                            }
+                        }
                     }
-                
-                    var helium = FindPackage(HeliumPackageName);
                 
                     if (!Version.TryParse(helium.version, out var versionInPackage))
                     {
@@ -78,23 +104,23 @@ namespace Editor
 
                     if (versionInAssets < versionInPackage)
                     {
-                        if (EditorUtility.DisplayDialog(
-                                HeliumWindowTitle,
-                                $"Samples/Dependencies for version {versionInAssets}, package is on version {versionInPackage}.\n\nDo you wish to update your existing Ad adapters?",
-                                "Yes", "No"))
-                        {
-                            Debug.LogError("Updating Dependencies");
-                        }
+                        var dialogInput = EditorUtility.DisplayDialog(
+                            HeliumWindowTitle,
+                            $"Samples/Dependencies for version {versionInAssets}, package is on version {versionInPackage}.\n\nDo you wish to update your existing Ad adapters?",
+                            "Yes", "No");
+                        
+                        if (dialogInput)
+                            ReimportExistingHeliumSamples(importedDependencies, helium.version);
                     }
                     else if (versionInAssets > versionInPackage)
                     {
-                        if (EditorUtility.DisplayDialog(
-                                HeliumWindowTitle,
-                                $"Samples/Dependencies for version {versionInAssets}, package is on version {versionInPackage}.\n\nDo you wish to downgrade your existing Ad adapters?\n\n**This is probably a bad setup, contact Helium Support**",
-                                "Yes", "No"))
-                        {
-                            Debug.LogError("Updating Dependencies");
-                        }
+                        var dialogInput = EditorUtility.DisplayDialog(
+                            HeliumWindowTitle,
+                            $"Samples/Dependencies for version {versionInAssets}, package is on version {versionInPackage}.\n\nDo you wish to downgrade your existing Ad adapters?\n\n**This is probably a bad setup, contact Helium Support**",
+                            "Yes", "No");
+                        
+                        if (dialogInput)
+                            ReimportExistingHeliumSamples(importedDependencies, helium.version);
                     }
                 }
             }
@@ -104,6 +130,21 @@ namespace Editor
                     HeliumWindowTitle, 
                     "No Samples directory found.\n\nMake sure to include at least the Helium dependencies.",
                     "Ok");
+            }
+            
+            void ReimportExistingHeliumSamples(ICollection<string> existingSamples, string version)
+            {
+                File.Delete(Path.Combine(HeliumSamplesInAssets, ".meta"));
+                Directory.Delete(HeliumSamplesInAssets, true);
+                AssetDatabase.Refresh();
+                            
+                var allSamples = Sample.FindByPackage(HeliumPackageName, version);
+                            
+                foreach (var sample in allSamples)
+                {
+                    if (existingSamples.Contains(sample.displayName))
+                        sample.Import(Sample.ImportOptions.HideImportWindow | Sample.ImportOptions.OverridePreviousImports);
+                }
             }
         }
     }
