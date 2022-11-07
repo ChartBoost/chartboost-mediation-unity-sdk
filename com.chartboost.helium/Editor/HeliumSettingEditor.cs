62a4bf53caf0d548a6c1b50641506ef5faf8fea8
SCastanedaMunoz <santiagocm98@hotmail.com>
2022-11-07 17:06:27 -0600

    Cleaning up HeliumSettingsEditor.cs

--- com.chartboost.helium/Editor/HeliumSettingEditor.cs
+++ com.chartboost.helium/Editor/HeliumSettingEditor.cs
@@ -9,14 +9,16 @@ namespace Helium.Editor
 	{
 		private const string AppIdLink = "https://dashboard.chartboost.com/all/publishing";
 
+		private readonly GUIContent _partnerKilLSwitchTitle = new GUIContent("Partner Kill Switch");
+		private readonly GUIContent _platformsIdsLabel = new GUIContent("Platform IDs");
 		private readonly GUIContent _iOSAppIdLabel = new GUIContent("App Id [?]:", "Helium App Ids can be found at " + AppIdLink);
 		private readonly GUIContent _androidAppIdLabel = new GUIContent("App Id [?]:", "Helium App Ids can be found at " + AppIdLink);
 		private readonly GUIContent _iOSAppSigLabel = new GUIContent("App Signature [?]:", "Helium App Signatures can be found at " + AppIdLink);
 		private readonly GUIContent _androidAppSigLabel = new GUIContent("App Signature [?]:", "Helium App Signatures can be found at " + AppIdLink);
 		private readonly GUIContent _iOSLabel = new GUIContent("iOS");
-		private readonly GUIContent _androidLabel = new GUIContent("Google Play");
-		private readonly GUIContent _enableLoggingLabel = new GUIContent("Enable Logging for Debug Builds");
-		private readonly GUIContent _enableAutomaticInitLabel = new GUIContent("Enable Automatic Initialization");
+		private readonly GUIContent _androidLabel = new GUIContent("Android");
+		private readonly GUIContent _debuggingTitle = new GUIContent("Debugging");
+		private readonly GUIContent _automaticInitLabel = new GUIContent("Automatic Initialization");
 		private readonly GUIContent _enableLoggingToggle = new GUIContent("isLoggingEnabled");
 		private readonly GUIContent _enableAutomaticInitToggle = new GUIContent("isAutomaticallyInitializing");
 
@@ -43,7 +45,7 @@ namespace Helium.Editor
 		{
 			// partner kill-switch
 			EditorGUILayout.BeginVertical();
-			EditorGUILayout.LabelField("Partner Kill Switch", _title);
+			EditorGUILayout.LabelField(_partnerKilLSwitchTitle, _title);
 			EditorGUILayout.Space();
 			EditorGUILayout.HelpBox("Select partners to disable their initialization.", MessageType.Info);
 			HeliumSettings.PartnerKillSwitch = (HeliumPartners)EditorGUILayout.EnumFlagsField(HeliumSettings.PartnerKillSwitch);
@@ -52,11 +54,11 @@ namespace Helium.Editor
 			EditorGUILayout.Space();
 			EditorGUILayout.Space();
 			
-			EditorGUILayout.LabelField("Platform IDs", _title);
+			EditorGUILayout.LabelField(_platformsIdsLabel, _title);
 			
 			EditorGUILayout.HelpBox("Add the Helium App Id & App Signature associated with this game.", MessageType.Info);
 			// iOS
-			EditorGUILayout.LabelField("iOS", _title);
+			EditorGUILayout.LabelField(_iOSLabel, _title);
 
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField(_iOSAppIdLabel);
@@ -80,7 +82,7 @@ namespace Helium.Editor
 
 			// Android
 			EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.LabelField("Android", _title);
+			EditorGUILayout.LabelField(_androidLabel, _title);
 			EditorGUILayout.EndHorizontal();
 
 			EditorGUILayout.BeginHorizontal();
@@ -103,13 +105,13 @@ namespace Helium.Editor
 			EditorGUILayout.Space();
 			EditorGUILayout.Space();
 			
-			EditorGUILayout.LabelField("Debugging", _title);
+			EditorGUILayout.LabelField(_debuggingTitle, _title);
 			EditorGUILayout.BeginHorizontal();
 			HeliumSettings.IsLoggingEnabled = EditorGUILayout.Toggle(_enableLoggingToggle, HeliumSettings.IsLoggingEnabled);
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.Space();
 			
-			EditorGUILayout.LabelField("Automatic Initialization", _title);
+			EditorGUILayout.LabelField(_automaticInitLabel, _title);
 			EditorGUILayout.BeginHorizontal();
 			HeliumSettings.IsAutomaticInitializationEnabled = EditorGUILayout.Toggle(_enableAutomaticInitToggle, HeliumSettings.IsAutomaticInitializationEnabled);
 			EditorGUILayout.EndHorizontal();
