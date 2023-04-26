e064a289f52d93323a61947192d806a8c50c538a
SCastanedaMunoz <santiagocm98@hotmail.com>
2023-04-26 17:45:37 -0500

    Adding Method to Update Mediation Version

--- com.chartboost.mediation/Editor/Adapters/AdaptersWindowUtilities.cs
+++ com.chartboost.mediation/Editor/Adapters/AdaptersWindowUtilities.cs
@@ -10,36 +10,6 @@ namespace Chartboost.Editor.Adapters
 {
     public partial class AdaptersWindow
     {
-        private static void Refresh(bool ignore = true)
-        {
-            if (!Application.isBatchMode)
-                Instance.rootVisualElement.Clear();
-            AdapterDataSource.Update();
-            Initialize();
-            if (!Application.isBatchMode|| !ignore)
-                EditorUtility.DisplayDialog("Chartboost Mediation", "Adapter update completed.", "ok");
-        }
-        
-        private static bool CheckForChanges()
-        {
-            var same = new DictionaryComparer<string, AdapterSelection>(new SelectedVersionsComparer()).Equals(UserSelectedVersions, SavedVersions);
-
-            if (Application.isBatchMode)
-                return same;
-
-            var root = Instance.rootVisualElement;
-            switch (same)
-            {
-                case false when _saveButton != null && !root.Contains(_saveButton):
-                    root.Add(_saveButton);
-                    break;
-                case true:
-                    _saveButton?.RemoveFromHierarchy();
-                    break;
-            }
-            return same;
-        }
-        
         public static List<AdapterSelection> AddNewNetworks(Platform platform, Func<string, Dictionary<string, AdapterSelection>, bool> condition)
         {
             var newNetworks = new List<AdapterSelection>();
@@ -125,32 +95,24 @@ namespace Chartboost.Editor.Adapters
             NoChangesDialog();
             return selectionChanges;
         }
-
-        private static bool WarningDialog()
+        
+        public static bool CheckChartboostMediationVersion()
         {
-            if (Application.isBatchMode)
-                return true;
+            if (!Application.isBatchMode && _warningButton != null)
+                _warningButton.RemoveFromHierarchy();
+            var package = Utilities.FindPackage(Constants.ChartboostMediationPackageName);
+
+            var version = new Version(MediationSelection);
+            var packageVersion = new Version(package.version);
+
+            if (!string.IsNullOrEmpty(MediationSelection) && Constants.PathToMainDependency.FileExist() &&
+                version == packageVersion) return false;
             
-            var cancel = false;
-            if (!Application.isBatchMode) { 
-                cancel = EditorUtility.DisplayDialog(
-                    "Chartboost Mediation", 
-                    "Doing this will update all of your selected adapters to their latest version, do you wish to continue?", "Yes", "No");
-            }
-            return cancel;
+            MediationSelection = package.version;
+            GenerateChartboostMediationDependency();
+            return true;
         }
-
-        private static void NoChangesDialog()
-        {
-            if (CheckForChanges() && !Application.isBatchMode)
-            {
-                EditorUtility.DisplayDialog(
-                    "Chartboost Mediation", 
-                    "No adapters updated, everything is already up to date!\n\n Do you think this is incorrect? Try using the refresh button.",
-                    "Ok");
-            }
-        }
-
+        
         private static void UpdateSelection(IReadOnlyList<string> versions, List<AdapterChange> selectionChanges,  string id, string startValue, Platform platform)
         {
             if (startValue.Equals(Constants.Unselected))
@@ -181,6 +143,61 @@ namespace Chartboost.Editor.Adapters
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(platform), platform, null);
+            }
+        }
+        
+        private static void Refresh(bool ignore = true)
+        {
+            if (!Application.isBatchMode)
+                Instance.rootVisualElement.Clear();
+            AdapterDataSource.Update();
+            Initialize();
+            if (!Application.isBatchMode|| !ignore)
+                EditorUtility.DisplayDialog("Chartboost Mediation", "Adapter update completed.", "ok");
+        }
+        
+        private static bool CheckForChanges()
+        {
+            var same = new DictionaryComparer<string, AdapterSelection>(new SelectedVersionsComparer()).Equals(UserSelectedVersions, SavedVersions);
+
+            if (Application.isBatchMode)
+                return same;
+
+            var root = Instance.rootVisualElement;
+            switch (same)
+            {
+                case false when _saveButton != null && !root.Contains(_saveButton):
+                    root.Add(_saveButton);
+                    break;
+                case true:
+                    _saveButton?.RemoveFromHierarchy();
+                    break;
+            }
+            return same;
+        }
+
+        private static bool WarningDialog()
+        {
+            if (Application.isBatchMode)
+                return true;
+            
+            var cancel = false;
+            if (!Application.isBatchMode) { 
+                cancel = EditorUtility.DisplayDialog(
+                    "Chartboost Mediation", 
+                    "Doing this will update all of your selected adapters to their latest version, do you wish to continue?", "Yes", "No");
+            }
+            return cancel;
+        }
+
+        private static void NoChangesDialog()
+        {
+            if (CheckForChanges() && !Application.isBatchMode)
+            {
+                EditorUtility.DisplayDialog(
+                    "Chartboost Mediation", 
+                    "No adapters updated, everything is already up to date!\n\n Do you think this is incorrect? Try using the refresh button.",
+                    "Ok");
             }
         }
     }
