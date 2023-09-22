844b89cf61c195d68a00525c0c2de2f6b47cc53f
Kushagra Gupta <imkushagra@gmail.com>
2023-09-21 19:57:31 -0700

    base file didn't get pushed

--- com.chartboost.mediation/Runtime/AdFormats/Banner/ChartboostMediationBannerViewBase.cs
+++ com.chartboost.mediation/Runtime/AdFormats/Banner/ChartboostMediationBannerViewBase.cs
@@ -33,7 +33,7 @@ namespace Chartboost.AdFormats.Banner
         public abstract BidInfo WinningBidInfo { get; protected set; }
         public abstract string LoadId { get; protected set; }
         public abstract Metrics? LoadMetrics { get; protected set; }
-        public abstract ChartboostMediationBannerAdSize? AdSize { get; protected set; }
+        public abstract ChartboostMediationBannerSize? AdSize { get; protected set; }
         public abstract ChartboostMediationBannerHorizontalAlignment HorizontalAlignment { get; set; }
         public abstract ChartboostMediationBannerVerticalAlignment VerticalAlignment { get; set; }
         
