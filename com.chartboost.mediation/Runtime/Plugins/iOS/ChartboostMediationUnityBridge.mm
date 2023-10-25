c7c88d3fbd07dc7386c1c403ffcd5884171c82d6
Ray Graham <rayg@bitwisedesign.com>
2023-10-25 10:30:10 -0700

    [HB-6642] Update Usage of Getting Screen Scale on iOS17+ (#195)

--- com.chartboost.mediation/Runtime/Plugins/iOS/ChartboostMediationUnityBridge.mm
+++ com.chartboost.mediation/Runtime/Plugins/iOS/ChartboostMediationUnityBridge.mm
@@ -616,7 +616,27 @@ char * _chartboostMediationGetUserIdentifier()
 }
 
 float _chartboostMediationGetUIScaleFactor() {
-    // TODO: https://github.com/ChartBoost/ios-helium-sdk/pull/1314
+    // `UIScreen.main` was deprecated in iOS 16. Apple doc:
+    //   https://developer.apple.com/documentation/uikit/uiscreen/1617815-main
+    // Since `UIScreen.main` has been working correctly at least up to iOS 16, the custom
+    // implementation only targets iOS 17+, not iOS 13+.
+    if (@available(iOS 17.0, *)) {
+        NSSet<UIScene*> *connectedScenes = UIApplication.sharedApplication.connectedScenes;
+        NSArray *activationStates = @[@(UISceneActivationStateForegroundActive), @(UISceneActivationStateForegroundInactive), @(UISceneActivationStateBackground), @(UISceneActivationStateUnattached)];
+        for (NSNumber *activationState in activationStates) {
+            UISceneActivationState state = (UISceneActivationState)activationState.integerValue;
+            for (UIScene* connectedScene in connectedScenes) {
+                UIWindowScene *windowScene = (UIWindowScene*)connectedScene;
+                if (windowScene) {
+                    if (windowScene.activationState == state) {
+                        return windowScene.screen.scale;
+                    }
+                }
+            }
+        }
+    }
+
+    // fallback
     return UIScreen.mainScreen.scale;
 }
 
