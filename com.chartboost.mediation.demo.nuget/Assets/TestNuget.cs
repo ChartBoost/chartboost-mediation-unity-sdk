adbbfb35682b3e4a9683dee782a5fdd46fea0a23
Kushagra Gupta <imkushagra@gmail.com>
2023-11-07 10:35:18 -0800

    project structure update

---
+++ com.chartboost.mediation.demo.nuget/Assets/TestNuget.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestNuget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{Directory.GetCurrentDirectory()}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ com.chartboost.mediation.demo.upm/Assets/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{Directory.GetCurrentDirectory()}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+    
